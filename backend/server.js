541d7e945cdf75f0e14efd6a1b2801bc74d85de4
tigmanshu-joshi <tigmanshujoshi21@gmail.com>
2023-06-03 14:12:13 +0530

    Database connectiont

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware.js');
 const connectDB = require('./config/db.js');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3001
 
 connectDB()
 
