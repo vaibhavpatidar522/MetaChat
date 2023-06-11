0dcbef18ae702e5f66f6ea01999892ac3daf31a7
tigmanshu-joshi <tigmanshujoshi21@gmail.com>
2023-06-11 22:50:20 +0530

    feat Chat-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import LoginPage from './pages/login-page';
 import LoginCredPage from './pages/login-cred';
 import SignUpPage from './pages/signup-page';
 import PinPage from './pages/pin-page';
-import ChatList from './pages/chat-list';
+import ChatList, { myName, userName2 } from './pages/chat-list';
+import ChatPage from './pages/chat-page';
 
 
 function App() {
   const loggedIn = true;
+  const chat_list_path = "/" + myName + userName2;
   return (
     <div className="App">
       <Routes>
@@ -18,6 +20,7 @@ function App() {
         <Route path='/login-details' element={<LoginCredPage/>}/>
         <Route path='/signup-page' element = {<SignUpPage/>}/>
         <Route path='/chat-list' element ={<ChatList/>}/>
+        <Route path={chat_list_path} element = {<ChatPage/>}/>
       </Routes>
     </div>
   );
